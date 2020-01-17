fa29323b21236d27e7685de18d45fde6402cbaf6
David Constante <celimoconstante@outlook.com>
2020-01-17 13:48:55 -0500

    .

--- Rentacar/Program.cs
+++ Rentacar/Program.cs
@@ -16,7 +16,7 @@ namespace Rentacar
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new FormAdmin());
+            Application.Run(new FormLogin());
         }
     }
 }
